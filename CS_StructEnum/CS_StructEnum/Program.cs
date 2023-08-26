468b67e69614b7bf974b9f8b735dcc88fb2830c0
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-26 23:22:59 +0700

    enum

---
+++ CS_StructEnum/CS_StructEnum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CS_StructEnum
+{
+    internal class Program
+    {
+        // kiểu liệt kê enum
+        /*
+         0 : kém 
+        1 : trung bình 
+        2 : khá 
+        3 : giỏi
+         */
+
+        enum HOCLUC
+        {
+            KEM = 1,
+            TRUNGBINH = 5,
+            KHA = 7,
+            GIOI = 10
+        }
+
+        // struct : // kiểu dữ liệu tham trị
+        public struct Product
+        {
+            // dữ liệu
+            public string name;
+            public string price;
+
+            // phương thức
+            // 
+            public string GetInfo()
+            {
+                return $"ten san pham {name}";
+            }
+            //constructor
+        }
+        static void Main(string[] args)
+        {
+            Product sanpham1;
+            HOCLUC hocluc;
+            hocluc  = HOCLUC.KEM;
+            Console.WriteLine(hocluc);
+        }
+    }
+}
