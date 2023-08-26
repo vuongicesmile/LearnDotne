3e64ff38fdc4b6ffa61587b8eab76e3da042e987
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-26 16:32:04 +0700

    class trong dotnet

---
+++ CS007_class/CS007_class/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CS007_class
+{
+    internal class Program
+    {
+        // class : kiểu dữ liệu tham chiếu
+
+        static void Main(string[] args)
+        {
+            VuKhi sungluc = new VuKhi();
+            sungluc.ThietLapDoSatThuong(5);
+            sungluc.TanCong();
+           // Console.WriteLine(sungluc); // console sẽ gọi thằng khởi tạo trước
+            VuKhi struong = new VuKhi("xin chao ");
+           // Console.WriteLine(struong);
+            // khai báo 1 phương thức khởi tạo khi khai báo in tên với độ sát thương luôn
+            VuKhi sungmay = new VuKhi("sung lung tung", 15);
+            sungmay.TanCong();
+            //  Console.WriteLine(sungmay);
+            // nếu tắt đi thuộc tính set thì sẽ không cho phép gán dữ liệu => câu lệnh bên dưới lỗi
+            sungluc.SatThuong = 10;
+            // nếu tắ đi thuộc tính get thì sẽ không cho phép đọc dữ liệu => nhưng gán lại được
+            Console.WriteLine(sungluc.SatThuong);
+
+            /*
+             Một class chỉ được khai báo 1 phương thức huỷ 
+            // khi không đủ bộ nhớ nó sẽ gọi đến phương thức huỷ
+            // thời điểm nào gọi phương thức huỷ này gọi thì do .net quyết định 
+
+             */
+        }
+    }
+}
---
+++ CS007_class/CS007_class/VuKhi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS007_class
+{
+    // cùng 1 chương trình internal : phạm vi truy cập mặc định
+    internal class VuKhi
+    {
+        // dữ liệu
+        string name = "Tên vũ khí"; // mặc định là prive => chỉ truy cập được ở phạm vi lớp
+        //=> muốn truy cập phạm vi bên ngoài phải dùng public
+
+        int dosatthuong = 0;
+
+        // thuộc tính
+        public int SatThuong
+        {
+            // thi hành khi thực hiện phép gán
+            set {
+                // 
+                Console.WriteLine("set");
+                dosatthuong = value;
+            }
+            // thi hành khi truy cập
+            get {
+                // phải trả về kiểu dữ liệu tương ưng với thuộc tính
+                return dosatthuong;
+            }
+        }
+        public string NoiSanXuat { get; set; }
+
+
+        // phương thức khởi tạo
+        // khai báo ra phương thức khởi tạo
+        // lưu ý : không có dữ liệu trả về
+        // mục đích : khi gọi từ khoá new để khởi tạo thì nó sẽ gọi trong phương thức đó đầu tiên
+        public VuKhi()
+        {
+            dosatthuong = 1;
+            Console.WriteLine("khoi tao");
+        }
+        // ta có thể quá tải hàm bằng cách đặt tên giống nhưng truyền params kiểu khác
+        public VuKhi(string abc)
+        {
+            Console.WriteLine("khoi tao" + abc);
+        }
+        public VuKhi(string name, int _dosatthuong)
+        {
+            this.name = name;
+            dosatthuong = _dosatthuong;
+        }
+        // phương thức 
+        public void ThietLapDoSatThuong(int dosatthuong)
+        {
+            // this : giá trị trỏ đến giá trị hiện tại của lớp
+            this.dosatthuong = dosatthuong;
+            
+            // this - ref
+            // VuKhi abc;
+            // abc = this;
+        }
+        public void TanCong()
+        {
+            Console.WriteLine(this.name);
+            for (int i = 0; i < dosatthuong; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
