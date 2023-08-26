43733fb726d0bab008bd659f31e977c0446b9326
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-26 11:33:35 +0700

    hello world

---
+++ CSHelloWorld/CSHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSHelloWorld
+{
+    internal class Program
+    {
+        // nhấn ctril f5 : để build 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
