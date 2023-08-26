9f01d85ca1fb270e6503884c069750211fbf2fbd
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-26 15:19:16 +0700

    methods

--- CS006_Methods/CS006_Methods/Program.cs
+++ CS006_Methods/CS006_Methods/Program.cs
@@ -4,12 +4,23 @@ namespace CS006_Methods
 {
     internal class Program
     {
+
+        class Count
+        {
+            public int c = 1;
+        }
+
         protected static void Main(string[] args)   
         {
             //CS006_Methods.TinhToan.tong(5, 6);
            var result = TinhToan.tong(5, 6);
             xinchao();
             Console.WriteLine(result);
+            Count count = new Count();
+          
+            Console.WriteLine(count.c);
+            dem(count);
+            Console.WriteLine(count.c); // khi mà 1 phương thức đc đi qua giá trị 1 lớp thì nó là tham chiếu không phải tham trị
         }
         // để truy cập đến xin chào thì phải truy cập đúng namespace đấy
         // <Access modifier> <return type> <name_method> (<parameters>) cú pháp khai báo 
@@ -22,6 +33,11 @@ namespace CS006_Methods
         {
             Console.WriteLine("Hello World!");
             //.. phương thức không có giá trị trả về
+        }
+
+        static void dem (Count count)
+        {
+            count.c++;
         }
 
     }
