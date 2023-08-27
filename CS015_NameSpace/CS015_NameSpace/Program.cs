61fb01dbd33425a78a9ace1690a541b332d2c927
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-27 14:32:50 +0700

    bài 16 : kiểu dữ liệu generic

--- CS015_NameSpace/CS015_NameSpace/Program.cs
+++ CS015_NameSpace/CS015_NameSpace/Program.cs
@@ -1,6 +1,8 @@
-﻿using CS015_NameSpace.Abc;
+﻿using CS015_NameSpace.Abc; // từ mynamespca
 using System;
+// khi muốn gọi namespace gọi từ khoá using
 
+// dùng từ khoá partial để chia nhỏ class ra nhiêu phần
 
 namespace CS015_NameSpace
 {
---
+++ CS016_ generic_Example/CS016_ generic_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CS016__generic_Example
+{
+    internal class Program
+    {
+        // giải thuật giống nhau nhưng kiểu dữ liệu khác nhau 
+        // => xử lý generic 
+
+        // khai báo kiểu đại diện T
+        static void Swap<T>(ref T x ,ref T y)
+        {
+            T t;
+            t = x;
+            x = y;
+            y = t;
+        }
+        static void Main(string[] args)
+        {
+            int a = 5; // khai báo là int thì t biến thành string
+            int b = 45;
+            Console.WriteLine($"a = {a} ., b = {b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"a = {a} ., b = {b}");
+
+        }
+    }
+}
