fe1185cc70a34efc4c49b21e7bf72be73ac0c09d
Nguyễn Quốc Vương <boide03@gmail.com>
2023-08-27 14:05:12 +0700

    namspcace

---
+++ CS015_NameSpace/CS015_NameSpace/Program.cs
@@ -0,0 +1,15 @@
+﻿using CS015_NameSpace.Abc;
+using System;
+
+
+namespace CS015_NameSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Abc.Class1.XinChao();
+
+        }
+    }
+}
---
+++ CS015_NameSpace/CS015_NameSpace/mynamespace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS015_NameSpace
+{
+    // class struct, enum ,interface .. cho phép khai báo các namespace con trongd dó
+    internal class mynamespace
+    {
+        public class Class1
+        {
+            public static void XinChao()
+            {
+                Console.WriteLine("xin chao từ class1");
+            }
+        }
+      
+    }
+    namespace Abc
+    {
+        public class Class1
+        {
+            public static void XinChao()
+            {
+                Console.WriteLine("xin chao từ class2");
+            }
+        }
+    }
+}
